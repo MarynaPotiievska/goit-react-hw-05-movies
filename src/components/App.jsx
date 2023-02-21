7450ca6d3f5db5d3f0ae8b840c1ce66bcd8a00aa
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-02-21 20:19:34 +0200

    added main pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+
+import SharedLayout from './SharedLayout';
+import MovieDetails from 'pages/MovieDetails';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={''} />
+            <Route path="reviews" element={''} />
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
