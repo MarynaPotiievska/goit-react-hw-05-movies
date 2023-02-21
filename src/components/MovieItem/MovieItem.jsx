85da259dbde9e843445949d42ee2ffe19a964bec
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-02-21 20:15:52 +0200

    fixed component MovieItem for correct linking

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import { FilmLink } from './MovieItem.styled';
 
-const MovieItem = ({ id, title }) => {
+const MovieItem = ({ id: movieId, title }) => {
+  const location = useLocation();
+  const isLocationFromHome = location.pathname === '/';
+
   return (
-    <li movieId={id}>
-      <FilmLink to="/movies/:movieId">{title}</FilmLink>
+    <li id={movieId}>
+      {isLocationFromHome ? (
+        <FilmLink to={`movies/${movieId}`} state={{ from: location }}>
+          {title}
+        </FilmLink>
+      ) : (
+        <FilmLink to={`${movieId}`} state={{ from: location }}>
+          {title}
+        </FilmLink>
+      )}
     </li>
   );
 };
